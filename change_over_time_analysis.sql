893ba6e1adf8bf5d35d9815694da1aa7c9ae045f
SIDDHANT JAIN <siddhantjainwork.sj@gmail.com>
2025-04-02 14:05:15 +0530

    Add files via upload

---
+++ change_over_time_analysis.sql
@@ -0,0 +1,33 @@
+SELECT
+    YEAR(order_date) AS order_year,
+    MONTH(order_date) AS order_month,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY YEAR(order_date), MONTH(order_date);
+
+-- DATETRUNC()
+SELECT
+    DATE_FORMAT(order_date, '%Y-%m-01') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATE_FORMAT(order_date, '%Y-%m-01')
+ORDER BY DATE_FORMAT(order_date, '%Y-%m-01');
+
+-- FORMAT()
+SELECT
+    DATE_FORMAT(order_date, '%Y-%b') AS order_date,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATE_FORMAT(order_date, '%Y-%b')
+ORDER BY DATE_FORMAT(order_date, '%Y-%b');
+
---
+++ cumulative_analysis.sql
@@ -0,0 +1,16 @@
+SELECT
+    order_date,
+    total_sales,
+    SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
+    AVG(avg_price) OVER (ORDER BY order_date) AS moving_average_price
+FROM
+(
+    SELECT 
+        DATE_FORMAT(order_date, '%Y-01-01') AS order_date,
+        SUM(sales_amount) AS total_sales,
+        AVG(price) AS avg_price
+    FROM gold.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY DATE_FORMAT(order_date, '%Y-01-01')
+) AS yearly_sales
+ORDER BY order_date;
